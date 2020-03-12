cbfe55ca641cdd718f7bc285e517f4d88b568eda
Gayda Denis Sergeevich <gayda31den@gmail.com>
2020-03-12 08:37:36 +0200

    dsfdsf

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -9,5 +9,5 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 export class OverviewComponent {
   public arr: Array<string> = ['Employee', 'Tech Interview Date', 'Before assessment level', 'Project', 'Project/Resource Manager', 'Interviewer'];
   public skills:  Array<string> = ['', 'J1', 'J2', 'J3', 'M1', 'M2', 'S', 'Ассесмент ранее не проводился', 'Trainee'];
-  public managers: string[] = ['', 'Василенко Виталий', 'Григорьев Александр', 'Педан Александр'];
+  public managers: Array<string> = ['', 'Василенко Виталий', 'Григорьев Александр', 'Педан Александр'];
 }

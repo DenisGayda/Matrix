c12747dca645a1410e64872b3fccbfcaeceae166
Oleksii Solomakha <alexssolomakha@gmail.com>
2020-03-13 12:17:48 +0200

    create 'select' component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
-import { OverviewComponent } from './components/overview/overview.component';
-import { OverviewHeaderComponent } from './components/overview-header/overview-header.component';
-import { HeaderRowComponent } from './components/header-row/header-row.component';
 import { SelectComponent } from './components/select/select.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
-    OverviewComponent,
-    OverviewHeaderComponent,
     SelectComponent,
-    HeaderRowComponent,
   ],
   imports: [
     BrowserModule,

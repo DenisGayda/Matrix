e98984cce2a44b30c2f830ae0a47e14364519726
kotetratata <kotetratata@gmail.com>
2020-03-13 15:27:36 +0200

    new branch to clear history

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,9 +1,31 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-table',
-  templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+@Injectable({
+  providedIn: 'root',
 })
-export class TableComponent {}
+export class TableDataService {
+  private url = 'assets/data.json';
+  constructor(private http: HttpClient) {}
+
+  public receiveData(sectionName: string): Observable<any> {
+    return this.http.get(this.url).pipe(
+      map( data => this.formatData(data[sectionName])));
+  }
+
+  private formatData(limb) {
+    let res = JSON.parse(JSON.stringify(limb));
+
+    Object.keys(res).map(
+      elem => {
+        if (typeof res[elem] === 'object') {
+          res[elem] = this.formatData(res[elem]);
+        } else {
+          res = Object.values(res);
+        }
+      });
+    return res;
+  }
+}

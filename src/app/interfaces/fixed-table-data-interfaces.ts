50ee25040aa6da4270b7061e0c2ef184026a9a0c
kotetratata <kotetratata@gmail.com>
2020-03-17 12:25:26 +0200

    added ful data to json and tryied to make interfaces

--- src/app/interfaces/fixed-table-data-interfaces.ts
+++ src/app/interfaces/fixed-table-data-interfaces.ts
@@ -1,5 +1,27 @@
 interface ISkillsData {
-  level: string[];
+  Level: string[];
 }
 
-export type tableData = string[] | ISkillsData;
+interface IGeneralAssessmentPoints {
+  'Software Engineering Practices': {
+    'Architecture knowledge': string[];
+    'Code Standards&Code Review Process': string[];
+    'Algorithms and data structures': string [];
+    'Continuous Integration&Continuous Delivery & Deployment': string[];
+    'Testing': string[];
+    'Troubleshooting/Logging/Monitoring': string[];
+  };
+  'Software Development Processes': {
+    'Version Control System(Git,tfs...)': string[];
+    'Estimations': string[];
+    'Development metodologies': string[];
+  };
+}
+
+interface IDevelopmentPoints {
+  'JS': string[];
+  'Framework Knowledges / Angular': string[];
+  'HTML&CSS': string[];
+}
+
+export type tableData = string[] | ISkillsData | IGeneralAssessmentPoints | IDevelopmentPoints;
--- src/assets/formatData.ts
+++ src/assets/formatData.ts
@@ -1,7 +1,7 @@
 import { jsonData } from '../app/interfaces/back-data-interfaces';
 import { tableData } from '../app/interfaces/fixed-table-data-interfaces';
 
-export function formatData(limb: jsonData): tableData {
+export function formatData(limb: tableData | jsonData): tableData {
   let result = Object.assign(limb);
 
   Object.keys(result).map(

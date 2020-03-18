ce19492585b7e7622f8627463be6e52e2c36b02a
bohdanov90 <aleksandr.bohdanov@gmail.com>
2020-03-18 15:07:38 +0200

    add text component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
+import { TextComponent } from './components/text/text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
+    TextComponent,
   ],
   imports: [
     BrowserModule,

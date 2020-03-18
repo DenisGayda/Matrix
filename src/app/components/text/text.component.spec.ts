2e7c3c07bc27ca4bfea2bad4085ee7617f559c17
bohdanov90 <aleksandr.bohdanov@gmail.com>
2020-03-18 15:31:16 +0200

    minor changes

--- src/app/components/text/text.component.spec.ts
+++ src/app/components/text/text.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TextComponent } from './text.component';
 
-describe('TextComponentComponent', () => {
+describe('TextComponent', () => {
   let component: TextComponent;
   let fixture: ComponentFixture<TextComponent>;
 

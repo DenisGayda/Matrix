97d15c7d7607a246d23f21d76abdfa1e22a59fae
LazarievAndrew <lazarev505@gmail.com>
2020-03-18 18:39:25 +0200

    fat-7697-outerComponent: create component outer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TableComponent } from './components/table/table.component';
+import { OuterComponent } from './components/outer/outer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TableComponent,
+    OuterComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/table/table.component.spec.ts
+++ src/app/components/table/table.component.spec.ts
@@ -1,11 +1,13 @@
 import { async, TestBed } from '@angular/core/testing';
 import { TableComponent } from './table.component';
+import { OuterComponent } from '../outer/outer.component';
 
 describe('TableComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         TableComponent,
+        OuterComponent,
       ],
     }).compileComponents();
   }));
